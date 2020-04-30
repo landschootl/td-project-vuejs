f1c114154dc47271c61fae4ab6c1550cda29ad71
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-04-30 16:26:47 +0200

    feat (save product): add create and update product

--- src/components/ManageProducts.vue
+++ src/components/ManageProducts.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="hello">
+  <div class="manage-product">
     <h1>La liste de mes produits :</h1>
     <div v-for="(product, index) in products" :key="index">
-      <span>{{product.name}} - </span><span>{{product.price}} €</span>
+      <span>{{product.name}} - </span>
+      <span>{{product.price}} €</span>
+      <span><button @click="updateProduct(product)">Editer le produit</button></span>
     </div>
+    <p>
+      <button @click="createProduct()">Créer un nouveau produit</button>
+    </p>
+    <SaveProduct :product="product" :isCreateMode="isCreateMode" @saveProduct="saveProduct($event)"></SaveProduct>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import SaveProduct from "./SaveProduct";
 
 export default {
   name: 'ManageProducts',
+  components: {
+    SaveProduct
+  },
   data() {
     return {
       backendUrl: 'https://node-baseapi.herokuapp.com/api',
-      products: []
+      products: [],
+      product: {name: 'default', price: 1.0}
     }
   },
   methods: {
@@ -25,8 +36,42 @@ export default {
                 this.products = response.data.result;
               })
               .catch(() => {
-                alert('impossible de récupérer les produits');
+                alert('Impossible de récupérer les produits');
               });
+    },
+    updateProduct(product) {
+      this.product = Object.assign({}, product);
+    },
+    createProduct() {
+      this.product = {name: 'default', price: 1.0};
+    },
+    saveProduct(product) {
+      if(this.isCreateMode) {
+        axios.post(`${this.backendUrl}/products`, product)
+                .then(result => {
+                  this.products.push(result.data.result);
+                  this.product = {name: 'default', price: 1.0};
+                })
+                .catch(() => {
+                  alert('Impossible de créer le produit');
+                });
+      } else {
+        axios.put(`${this.backendUrl}/products/${product.id}`, product)
+                .then(() => {
+                  this.products = this.products.map(p => {
+                    return p.id === product.id ? Object.assign({}, product) : p;
+                  });
+                  this.product = {name: 'default', price: 1.0};
+                })
+                .catch(() => {
+                  alert('Impossible de modifier le produit');
+                });
+      }
+    }
+  },
+  computed: {
+    isCreateMode() {
+      return this.product.id === undefined;
     }
   },
   created() {
---
+++ src/components/SaveProduct.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="save-product">
+    <h1>{{isCreateMode ? 'Créer un nouveau produit' : 'Modifier le produit'}}</h1>
+    <form @submit.prevent="submitProduct()">
+      <input type="text" v-model="product.name"/>
+      <input type="text" v-model.number="product.price"/>
+      <button type="submit">Sauvegarder le produit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'SaveProduct',
+  props: {
+    isCreateMode: Boolean,
+    product: Object
+  },
+  methods: {
+    submitProduct() {
+      this.$emit('saveProduct', this.product);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
